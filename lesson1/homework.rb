21a86603c79cfb9225eecebe4a8d8097243543ef
Stepan Tischenko <stepantishhen@mail.ru>
2023-09-10 16:33:47 +0300

    Update homework.rb

--- lesson1/homework.rb
+++ lesson1/homework.rb
@@ -7,8 +7,9 @@ response = HTTParty.get(url)
 
 # получаем рандомный анекдот
 if response.code != 200
-    puts 'не удалось получить данные с веб-сайта'
-  else
-    random_number = rand(0..10)
-    doc = Nokogiri::HTML(response.body)
-    puts doc.css('.text')[random_number].text
+  puts 'не удалось получить данные с веб-сайта'
+else
+  random_number = rand(0..10)
+  doc = Nokogiri::HTML(response.body)
+  puts doc.css('.text')[random_number].text
+end
